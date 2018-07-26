728b267ec47f6d0273d949e73b5a3070a5d7002a
wtkk <1300164405@qq.com>
2018-07-26 17:24:33 +0800

    初始化版本库

---
+++ src/main/java/com/wtkk/little_life/LittleLifeApplication.java
@@ -0,0 +1,12 @@
+package com.wtkk.little_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleLifeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wtkk/little_life/LittleLifeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wtkk.little_life;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LittleLifeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
